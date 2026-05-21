2812988732ca32fb02106aabba719a300984e41d
rampop01 <popoolarahmat96@gmail.com>
2026-05-21 19:44:03 +0100

    feat: scaffold FilterChips component for categories and status filters

---
+++ src/components/FilterChips.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import React from 'react';
+import styles from './FilterChips.module.css';
+
+export interface FilterOption {
+  id: string;
+  label: string;
+}
+
+interface FilterChipsProps {
+  options: FilterOption[];
+  activeId: string;
+  onChange: (id: string) => void;
+}
+
+export default function FilterChips({ options, activeId, onChange }: FilterChipsProps) {
+  return (
+    <div className={styles.filterContainer}>
+      {options.map((option) => (
+        <button
+          key={option.id}
+          type="button"
+          className={`${styles.chip} ${activeId === option.id ? styles.chipActive : ''}`}
+          onClick={() => onChange(option.id)}
+        >
+          {option.label}
+        </button>
+      ))}
+    </div>
+  );
+}
